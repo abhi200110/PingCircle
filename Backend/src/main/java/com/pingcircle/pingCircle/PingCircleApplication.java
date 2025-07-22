8b7ecbf70b660525caeaaa2805d1fd76209210ef
Abhijit <abhijitghosh2848@gmail.com>
2025-07-22 15:46:49 +0530

    setup
    setup

---
+++ Backend/src/main/java/com/pingcircle/pingCircle/PingCircleApplication.java
@@ -0,0 +1,13 @@
+package com.pingcircle.pingCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingCircleApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/pingcircle/pingCircle/PingCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingcircle.pingCircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
